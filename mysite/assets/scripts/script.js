b44382ad6eff14d9451f14eaf53a2007c1f0b8d3
RyanPMcIntosh <mcintoshryanp@gmail.com>
2024-04-18 14:21:44 +1000

    making changes

---
+++ mysite/assets/scripts/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const areas = document.querySelectorAll('area');
+    const infoBox = document.getElementById('info-box-boat');
+  
+    areas.forEach(area => {
+      area.addEventListener('click', function() {
+        const info = this.dataset.info;
+        infoBox.textContent = info;
+        infoBox.style.display = 'block';
+      });
+    });
+  
+    document.getElementById('image-boat').addEventListener('click', function(event) {
+      // Hide info box if clicked outside of area
+      if (event.target.tagName !== 'AREA') {
+        infoBox.style.display = 'none';
+      }
+    });
+  });
